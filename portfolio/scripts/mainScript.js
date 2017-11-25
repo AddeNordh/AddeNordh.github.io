d633075ce5875e25b6e956ef6880211282711c70
AddeNordh <nordh.andreas@hotmail.com>
2017-11-25 21:49:51 +0100

    Reworked typeword func to be more reusable

--- portfolio/scripts/mainScript.js
+++ portfolio/scripts/mainScript.js
@@ -25,24 +25,30 @@ const addClass = (elementsArray, delay, element, classToAdd) => {
 
 
 
-const greetmsg = "Hello, my name is <b>Andreas Nordh</b> I am a soon to be fullstack developer.<br> Currentely a freshman year studen at <b> YRGO Gothenburg Sweden. </b><br> Feel free to take a look at my <b><a href='github.com/addenordh'> Github.</a></b> <br>Also you can take a contact me via <b>Nordh.Andreas@hotmail.com</b>.";
+const greetmsg = "Hello, my name is <b>Andreas Nordh</b> I am a soon to be fullstack developer.<br> Currentely a freshman year studen at <b> YRGO Gothenburg Sweden. </b><br> Feel free to take a look at my <b><a href='https://github.com/addenordh' target='_blank'> Github.</a></b> <br>Also you can take a contact me via <b>Nordh.Andreas@hotmail.com</b>.";
 let number = 0;
 const field = document.getElementById("greet-msg");
 
-const typeWord = () => {
+const typeWord = (word, delay) => {
 	let text = greetmsg.slice(0,++number);
 	field.innerHTML = text;
 	if (text != greetmsg) {
-	  return setTimeout(typeWord, 15);
+	  return setTimeout(() => {
+		  typeWord(word, delay);
+	  }, delay);
 	}
 }
 
-setTimeout(typeWord, 400);
+setTimeout(() => {
+	typeWord(greetmsg,15);
+},400);
+
+
+
 
 const menuIcon = document.getElementById('nav-icon');
 const nav = document.getElementById('nav');
 const navItems = nav.getElementsByClassName('nav-item');
-console.log(navItems);
 
 menuIcon.addEventListener("click", () => {
 	if (!menuIcon.classList.contains("nav-toggled")) {
