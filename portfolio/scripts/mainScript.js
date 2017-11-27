c6b0a4aa2acc390bebe94b5089ae78bb4b6ecb93
AddeNordh <nordh.andreas@hotmail.com>
2017-11-27 15:54:41 +0100

    Updated write sentance func and more
    Updated the writh sentacen function to be able to write from an array.
    Also removed all the "content" function along with the next function to refactor it

--- portfolio/scripts/mainScript.js
+++ portfolio/scripts/mainScript.js
@@ -1,32 +1,23 @@
 
 "use strict";
-let sectionContent;
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 const pages = document.getElementsByTagName('main');
 let pageIndex = 0;
 const homeContentItems = document.getElementsByClassName('home-content');
-const greetmsg = "Hello, I'm <b>Andreas Nordh</b> and I am a soon to be fullstack developer.<br> Currently a freshman year student at <b> YRGO Gothenburg Sweden. </b><br> Feel free to take a look at my <b><a href='https://github.com/addenordh' target='_blank'> Github</a></b> to view some of my work <br>Also you can take a contact me via <b>Nordh.Andreas@hotmail.com</b><span class='__dot'>.</span>";
+const greetmsg = ["Hello, I'm <b>Andreas Nordh</b> and I am a soon to be fullstack developer.", "Currently a freshman year student at <b>YRGO Gothenburg Sweden. </b><br>", "Feel free to take a look at my <a href='https://github.com/addenordh' target='_blank'> Github</a></b> to view some of my work <br>","Also you can take a contact me via <b>Nordh.Andreas@hotmail.com</b><span class='__dot'>.</span>"];
 let number = 0;
-const field = document.getElementById("greet-msg");
+const fields = Array.from(document.getElementsByClassName('greet-msg'));
 const menuIcon = document.getElementById('nav-icon');
 const nav = document.getElementById('nav');
 const navItems = nav.getElementsByClassName('nav-item');
+const navBars = document.getElementsByClassName('nav-bar');
 const workItems = document.getElementsByClassName('work-item');
-
-for (let workItem of workItems) {
-	workItem.addEventListener("mouseover", () => {
-		let img = workItem.querySelector("img");
-		let src = img.src;
-		let itemInfo = workItem.querySelector(".work-item-info");
-		itemInfo.style.background = "url(" + src + ")";
-		itemInfo.style.backgroundSize = "cover";
-	});
-}
-
 
 
 const addClass = (elementsArray, delay, element, classToAdd, mult) => {
+	mult = mult || 1;
+	delay = delay || 100;
     if (elementsArray) {
         for (var i = 0; i < elementsArray.length; i++) {
         ((i) => {
@@ -40,12 +31,14 @@ const addClass = (elementsArray, delay, element, classToAdd, mult) => {
 		((classToAdd) => {
 			setTimeout(() => {
 				element.classList.add(classToAdd);
-			},delay);
+			},delay * mult);
 		})(classToAdd);
     }
 }
 
 const removeClass = (elementsArray, element, delay, classToRemove, mult) => {
+	mult = mult || 1;
+	delay = delay || 100;
     if (elementsArray) {
         for (var i = 0; i < elementsArray.length; i++) {
         ((i) => {
@@ -59,21 +52,36 @@ const removeClass = (elementsArray, element, delay, classToRemove, mult) => {
 		((classToRemove) => {
 			setTimeout(() => {
 				element.classList.remove(classToRemove);
-			},delay);
+			},delay * mult);
 		})(classToRemove);
     }
 }
 
-
-const typeWord = (word, delay) => {
-	let text = greetmsg.slice(0,++number);
-	field.innerHTML = text;
-	if (text != greetmsg) {
-	  return setTimeout(() => {
-		  typeWord(word, delay);
-	  }, delay);
+function typeWord(msg, delay, fields) {
+	this.msg = msg;
+	this.delay = delay;
+	this.count = 0;
+	this.index = 0;
+	this.field = fields[this.index];
+	this.sentance = this.msg[this.index];
+	this.type = function(text, field, delay) {
+		this.text = this.sentance.slice(0, this.count++);
+		this.field.innerHTML = this.text;
+		if (this.text != this.sentance) {
+			return setTimeout(() => {
+				this.type(text, field, 25);
+			}, delay);
+		}
+		else {
+			this.count = 0;
+			this.index++;
+			this.field = fields[this.index];
+			this.sentance = this.msg[this.index];
+			this.type(this.sentance, this.field, 25);
+		}
 	}
 }
+
 
 menuIcon.addEventListener("click", () => {
 	if (!menuIcon.classList.contains("nav-toggled")) {
@@ -88,92 +96,30 @@ menuIcon.addEventListener("click", () => {
 	}
 });
 
-const arrowLines = document.getElementsByClassName('line');
+const rightArrow = document.getElementsByClassName('right-line');
 setTimeout(() => {
-	addClass(arrowLines, 200, null, "active");
+	addClass(navBars, 150, null, "active");
+	addClass(rightArrow, 200, null, "active");
+	let sentace = new typeWord(greetmsg, 15, fields);
+	sentace.type(greetmsg[0],fields[0], 25);
 },1000);
 
-const getContent = (section) => {
-	let sectionContent = [];
-	let contentClasses = [];
-	let sectionItems = section.querySelectorAll("div");
-	for (let item of sectionItems) {
-		let classes = Array.from(item.classList);
-		classes.forEach((c) => {
-			if (c === "su-fi" || c === "si-f-r") {
-				sectionContent.push(item);
-				contentClasses.push(c);
-			}
-		});
-	}
-	return {
-		section : section,
-		items : sectionContent,
-		classes : contentClasses
-	}
-}
-
-const removeContent = (obj) => {
-	let item = null;
-	let Class = null;
-	let cta = null;
-	for (let i = 0; i < obj.items.length; i++) {
-		item = obj.items[i];
-		Class = obj.classes[i];
-		cta = null;
-		if (Class === "su-fi") {
-			cta = "slideUpp-FadeIn";
-		}
-		if (Class === "si-f-r") {
-			cta = "slideIn-f-right"
-		}
-		removeClass(null, item, 200, cta);
-	}
-}
-
-const addContent = (obj, delay) => {
-	let item = null;
-	let Class = null;
-	let cta = null;
-	let count = 1;
-	for (let i = 0; i < obj.items.length; i++) {
-		item = obj.items[i];
-		Class = obj.classes[i];
-		cta = null;
-		if (Class === "su-fi") {
-			cta = "slideUpp-FadeIn";
-		}
-		if (Class === "si-f-r") {
-			cta = "slideIn-f-right"
-		}
-		addClass(null, delay * (count + 1), item, cta, count);
-		count++;
-	}
-}
 
 
 next.addEventListener("click", () => {
-	let section = pages[pageIndex];
-	let content = getContent(section);
-	removeContent(content);
-
-	removeClass(null, section, 700, "active");
-	addClass(null, 700, section, "prev");
-	let nextSection = pages[++pageIndex];
-	removeClass(null, nextSection, 700, "next");
-	addClass(null, 700, nextSection, "active");
-
-	let nextContent = getContent(nextSection);
-	setTimeout(() => {
-		addContent(nextContent, 400);
-	}, 1300)
 });
 
 
-addClass(homeContentItems, 300, null, "slideUpp-FadeIn");
-addClass(null, 300, menuIcon, "slideIn-f-rigth-fadeIn-2");
-setTimeout(() => {
-	typeWord(greetmsg,15);
-},1100);
+addClass(homeContentItems, 300, null, "active");
+addClass(null, 300, menuIcon, "active");
 
-getContent(pages[pageIndex]);
+
+for (let workItem of workItems) {
+	workItem.addEventListener("mouseover", () => {
+		let img = workItem.querySelector("img");
+		let src = img.src;
+		let itemInfo = workItem.querySelector(".work-item-info");
+		itemInfo.style.background = "url(" + src + ")";
+		itemInfo.style.backgroundSize = "cover";
+	});
+}
