842b589a74d266bce6408375205b0c2cab2aec0d
AddeNordh <nordh.andreas@hotmail.com>
2017-11-28 15:03:11 +0100

    Added bold tag to github link

--- portfolio/scripts/mainScript.js
+++ portfolio/scripts/mainScript.js
@@ -5,7 +5,7 @@ const next = document.getElementById('next');
 const pages = Array.from(document.querySelectorAll('.content'));
 let pageIndex = 0;
 const homeContentItems = document.getElementsByClassName('home-content');
-const greetmsg = ["Hello, I'm <b>Andreas Nordh</b> and I am a soon to be fullstack developer.", "Currently a freshman year student at <b>YRGO Gothenburg Sweden. </b><br>", "Feel free to take a look at my <a href='https://github.com/addenordh' target='_blank'> Github</a></b> to view some of my work <br>","Also you can take a contact me via <b>Nordh.Andreas@hotmail.com</b><span class='__dot'>.</span>"];
+const greetmsg = ["Hello, I'm <b>Andreas Nordh</b> and I am a soon to be fullstack developer.", "Currently a freshman year student at <b>YRGO Gothenburg Sweden. </b><br>", "Feel free to take a look at my <b><a href='https://github.com/addenordh' target='_blank'> Github</a></b> to view some of my work <br>","Also you can take a contact me via <b>Nordh.Andreas@hotmail.com</b><span class='__dot'>.</span>"];
 const fields = Array.from(document.getElementsByClassName('greet-msg'));
 const menuIcon = document.getElementById('nav-icon');
 const nav = document.getElementById('nav');
